97ec272eb764734acd307a94eab38bec9ea86a4e
r123-dev <rohanmourya879@gmail.com>
2025-03-30 00:00:36 +0530

    project

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const app = require('./app');
+const app = require('./src/app');
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
